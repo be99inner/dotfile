5082acec166f7f64397e0ccb6ca08cb31e8b1f3c
be99inner <llawliet.solamoon@gmail.com>
2019-06-12 11:27:12 +0700

    switch keyboard layout

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 exec nm-applet &
+
+# Telegram
+exec /opt/Telegram/Telegram &
+
+# Pulseaudio
+exec pulseaudio -d
+
+# Set keyboard Switching
+exec setxkbmap us,th -option 'grp:lalt_lshift_toggle'
