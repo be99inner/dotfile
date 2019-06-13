5bd18cf0eac9526157889107b1d748b30260bd7f
be99inner <llawliet.solamoon@gmail.com>
2019-06-13 13:54:47 +0700

    fix key to motivation

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -11,12 +11,12 @@ mod = "mod4"
 
 keys = [
     # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down()),
-    Key([mod], "j", lazy.layout.up()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod], "j", lazy.layout.down()),
 
     # Move windows up or down in current stack
-    Key([mod, "control"], "k", lazy.layout.shuffle_down()),
-    Key([mod, "control"], "j", lazy.layout.shuffle_up()),
+    Key([mod, "control"], "k", lazy.layout.shuffle_up()),
+    Key([mod, "control"], "j", lazy.layout.shuffle_down()),
 
     # Switch window focus to other pane(s) of stack
     Key([mod], "s", lazy.layout.next()),
