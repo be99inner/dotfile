e46afc6e0c2f1c7e684e068722549a4294889e27
be99inner <anurak.jann@gmail.com>
2022-08-09 19:35:09 +0700

    nvim: fix comment config

--- config/nvim/lua/motivation.lua
+++ config/nvim/lua/motivation.lua
@@ -17,7 +17,7 @@ require('guess-indent').setup {
 require('nvim_comment').setup({
   hook = function()
     -- Set comment string for Terraform
-    if vim.api.nvim_buf_get_option(0, "filetypes") == "terraform" then
+    if vim.api.nvim_buf_get_option(0, "filetype") == "terraform" then
       vim.api.nvim_buf_set_option(0, "commentstring", "# %")
     end
   end
