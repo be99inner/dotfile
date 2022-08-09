ec9c0bd5591c8038829e1fc39c4af4cd92b75039
be99inner <anurak.jann@gmail.com>
2022-08-09 21:43:34 +0700

    nvim: fix comment string

--- config/nvim/lua/motivation.lua
+++ config/nvim/lua/motivation.lua
@@ -18,7 +18,7 @@ require('nvim_comment').setup({
   hook = function()
     -- Set comment string for Terraform
     if vim.api.nvim_buf_get_option(0, "filetype") == "terraform" then
-      vim.api.nvim_buf_set_option(0, "commentstring", "# %")
+      vim.api.nvim_buf_set_option(0, "commentstring", "# %s")
     end
   end
 })
