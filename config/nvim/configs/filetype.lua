20cc1703916e918215e8575e9a82b6396de9c904
be99inner <anurak.jann@gmail.com>
2024-01-04 22:56:30 +0700

    fix(nvim): fix filetype

--- config/nvim/configs/filetype.lua
+++ config/nvim/configs/filetype.lua
@@ -1,1 +1,13 @@
-require("filetype").setup()
+require("filetype").setup {
+  overrides = {
+    extensions = {
+      tf = "terraform",
+      tfvars = "terraform",
+      tfstate = "json",
+      sh = "sh",
+    },
+    complex = {
+      ["/%.kube/config"] = "yaml",
+    },
+  },
+}
