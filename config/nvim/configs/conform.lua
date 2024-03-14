f81bb6ce5583d01f3a643a2c4b3fd82485229593
be99inner <anurak.jann@gmail.com>
2024-03-15 00:12:31 +0700

    fix(nvim): fix format on save

--- config/nvim/configs/conform.lua
+++ config/nvim/configs/conform.lua
@@ -9,6 +9,8 @@ local options = {
     javascript = { "prettier" },
     css = { "prettier" },
     html = { "prettier" },
+    yaml = { "prettier" },
+    json = { "prettier" },
 
     -- shell
     sh = { "shfmt" },
@@ -23,11 +25,11 @@ local options = {
   -- adding same formatter for multiple filetypes can look too much work for some
   -- instead of the above code you could just use a loop! the config is just a table after all!
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 2000,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 2000,
+  --   lsp_fallback = true,
+  -- },
   -- If this is set, Conform will run the formatter asynchronously after save.
   -- It will pass the table to conform.format().
   -- This can also be a function that returns the table.
