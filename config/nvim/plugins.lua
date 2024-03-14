2ee91af64b561d2f09b582e68be5b43beccfd740
be99inner <anurak.jann@gmail.com>
2024-03-15 00:13:17 +0700

    feature(nvim): add plugins for motivation

--- config/nvim/plugins.lua
+++ config/nvim/plugins.lua
@@ -97,6 +97,23 @@ local plugins = {
   -- editorconfig
   { "gpanders/editorconfig.nvim", lazy = false },
 
+  -- motivation
+  {
+    "kylechui/nvim-surround",
+    -- version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  -- multi-cursor
+  {
+    "mg979/vim-visual-multi",
+    event = "VeryLazy",
+  },
+
   -- lsp installer
   {
     "williamboman/mason.nvim",
