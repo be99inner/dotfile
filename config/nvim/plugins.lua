56f154686a6c25c4fe0b92bce5ae4c7f80a036e5
be99inner <anurak.jann@gmail.com>
2024-03-25 22:53:32 +0700

    chore(nvim): remove noice for noti
    Signed-off-by: be99inner <anurak.jann@gmail.com>

--- config/nvim/plugins.lua
+++ config/nvim/plugins.lua
@@ -1,24 +1,5 @@
 -- Plugins Tables
 local plugins = {
-  -- better notification
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require "custom.configs.noice"
-    end,
-  },
   -- smooth scrolling
   {
     "declancm/cinnamon.nvim",
