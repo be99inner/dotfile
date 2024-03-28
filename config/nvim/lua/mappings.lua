3828c8d571941e697e2a92f366e0639af1bd73b7
be99inner <anurak.jann@gmail.com>
2024-03-28 21:12:04 +0700

    fix(nvim): fix tmux-integration
    Signed-off-by: be99inner <anurak.jann@gmail.com>

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -21,3 +21,9 @@ map({ "v", "x" }, "<A-k>", ":move '<-2<CR>gv-gv", { desc = "Move Text Up" })
 
 -- fast to save
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- tmux integration
+map("n", "<C-h>", require("nvim-tmux-navigation").NvimTmuxNavigateLeft)
+map("n", "<C-l>", require("nvim-tmux-navigation").NvimTmuxNavigateRight)
+map("n", "<C-j>", require("nvim-tmux-navigation").NvimTmuxNavigateDown)
+map("n", "<C-k>", require("nvim-tmux-navigation").NvimTmuxNavigateUp)
