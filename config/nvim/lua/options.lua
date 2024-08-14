2dc72f0180dab0671f93187ca6fcb966e34d6b13
be99inner <anurak.jann@gmail.com>
2024-08-14 10:41:40 +0700

    feat(nvim): add folding config
    Signed-off-by: be99inner <anurak.jann@gmail.com>

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -10,3 +10,11 @@ opt.relativenumber = true
 opt.scrolloff = 4
 opt.sidescrolloff = 4
 opt.eol = true
+
+-- folding
+opt.foldenable = true
+opt.foldmethod = "expr"
+opt.foldcolumn = "1" -- easy to visual which line is folded
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 20
+opt.foldlevelstart = 12
