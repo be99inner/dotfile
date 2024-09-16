681f71a959a257c52566a1931f810c5b97baa01f
be99inner <anurak.jann@gmail.com>
2024-09-17 00:49:00 +0700

    feat(nvim): add tmux integration

--- config/nvim-vanila/lua/plugins/extras/utils.lua
+++ config/nvim-vanila/lua/plugins/extras/utils.lua
@@ -1,3 +1,24 @@
-local plugins = {}
+local plugins = {
+  -- Tmux
+  {
+    "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    cmd = { "NvimTmuxNavigateLeft", "NvimTmuxNavigateRight", "NvimTmuxNavigateUp", "NvimTmuxNavigateDown" },
+    -- config = function()
+    --   require("nvim-tmux-navigation").setup({
+    --     disable_when_zoomed = true, -- defaults to false
+    --   })
+    -- end,
+    opts = {
+      disabled_when_zoomed = true,
+    },
+    keys = {
+      { "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", { desc = "Tmux Navigate Left", silent = false } },
+      { "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", { desc = "Tmux Navigate Down", silent = false } },
+      { "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", { desc = "Tmux Navigate Up", silent = false } },
+      { "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", { desc = "Tmux Navigate Right", silent = false } },
+    },
+  },
+}
 
 return plugins
