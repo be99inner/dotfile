5bd49995729b323ea6123fa899491213ec7888d9
be99inner <anurak.jann@gmail.com>
2024-09-16 11:34:18 +0700

    feat(nvim): add lazygit
    Signed-off-by: be99inner <anurak.jann@gmail.com>

---
+++ config/nvim-vanila/lua/plugins/extras/git.lua
@@ -0,0 +1,21 @@
+local plugins = {
+  -- Lazy.git
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
+
+return plugins
