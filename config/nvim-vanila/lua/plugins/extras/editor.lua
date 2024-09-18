4e1ef87cfc0e57ef2905f4f07b93866cde269d29
be99inner <anurak.jann@gmail.com>
2024-09-18 12:35:36 +0700

    feat(nvim): add search and replace plugin
    Signed-off-by: be99inner <anurak.jann@gmail.com>

--- config/nvim-vanila/lua/plugins/extras/editor.lua
+++ config/nvim-vanila/lua/plugins/extras/editor.lua
@@ -54,6 +54,22 @@ local plugins = {
     },
   },
 
+  -- Search and Replace
+  {
+    "MagicDuck/grug-far.nvim",
+    cmd = "GrugFar",
+    config = function()
+      require("grug-far").setup({})
+    end,
+    keys = {
+      {
+        "<leader>sr",
+        "<cmd>GrugFar<cr>",
+        desc = "Search and Replace",
+      },
+    },
+  },
+
   -- Enrich motion on searching
   {
     "folke/flash.nvim",
