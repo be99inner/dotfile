f767c302feb5e700a3eae0c670e1804b4ae1377c
be99inner <anurak.jann@gmail.com>
2024-09-24 21:35:31 +0700

    fix(nvim): add disabled_plugins to lazy.nvim

--- config/nvim-vanila/lua/configs/core/lazy.lua
+++ config/nvim-vanila/lua/configs/core/lazy.lua
@@ -11,6 +11,40 @@ local opts = {
   install = { colorscheme = { "tokyonight", "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = false },
+
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+      },
+    },
+  },
 }
 
 return opts
