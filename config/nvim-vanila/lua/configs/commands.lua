ab44ad5e15680c70ff06fed234ebde7282480335
be99inner <anurak.jann@gmail.com>
2024-09-24 18:38:02 +0700

    feat(nvim): add command to remember the last place before exit

---
+++ config/nvim-vanila/lua/configs/commands.lua
@@ -0,0 +1,15 @@
+-- remember last place before quit nvim
+local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
+vim.api.nvim_clear_autocmds({ group = lastplace })
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = lastplace,
+  pattern = { "*" },
+  desc = "remember last cursor place",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
