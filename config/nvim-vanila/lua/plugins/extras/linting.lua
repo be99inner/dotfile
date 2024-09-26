ad0f4d829a8c09f5087bb1fdeed0abd974121be2
be99inner <anurak.jann@gmail.com>
2024-09-26 11:40:37 +0700

    feat(nvim): add linting

--- config/nvim-vanila/lua/plugins/extras/linting.lua
+++ config/nvim-vanila/lua/plugins/extras/linting.lua
@@ -1,3 +1,29 @@
-local plugins = {}
+local plugins = {
+  {
+    "mfussenegger/nvim-lint",
+    event = "BufWritePost",
+    config = function()
+      require("lint").linters_by_ft = {
+        -- markdown = { "vale" },
+        terraform = { "tfsec" },
+      }
+
+      -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      --   callback = function()
+      --     -- try_lint without arguments runs the linters defined in `linters_by_ft`
+      --     -- for the current filetype
+      --     require("lint").try_lint()
+      --
+      --     -- You can call `try_lint` with a linter name or a list of names to always
+      --     -- run specific linters, independent of the `linters_by_ft` configuration
+      --     -- require("lint").try_lint("cspell")
+      --   end,
+      -- })
+    end,
+    keys = {
+      { "<leader>cL", mode = { "n" }, function() require("lint").try_lint() end, desc = "Linting current buffer" },
+    },
+  },
+}
 
 return plugins
