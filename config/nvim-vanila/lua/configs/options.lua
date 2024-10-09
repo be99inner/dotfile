07c95568f27626f5da4de390aa5c756ab13eec3d
be99inner <anurak.jann@gmail.com>
2024-10-09 22:11:23 +0700

    fix(nvim): fix vim options

--- config/nvim-vanila/lua/configs/options.lua
+++ config/nvim-vanila/lua/configs/options.lua
@@ -1,32 +1,74 @@
-local opt = vim.opt
-local o = vim.o
-local g = vim.g
+-- Enable Editorconfig
+vim.g.editorconfig = true
 
-------------------------------------- global ---------------------------------
-g.editorconfig = true -- enable Editorconfig
+-- Enable Term Color
+vim.opt.termguicolors = true
 
-------------------------------------- option ---------------------------------
-opt.termguicolors = true -- terminal gui colors
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function() vim.opt.clipboard = "unnamedplus" end)
 
-o.updatetime = 250
-o.laststatus = 3
+-- Indenting setup
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
-o.clipboard = "unnamedplus" -- clipboard
-o.cursorline = true -- cursorline
-o.cursorlineopt = "number"
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
 
--- Indenting
-o.expandtab = true
-o.shiftwidth = 2
-o.smartindent = true
-o.tabstop = 2
-o.softtabstop = 2
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
 
-opt.fillchars = { eob = " " }
-o.ignorecase = true
-o.smartcase = true
-o.mouse = "a"
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+vim.opt.relativenumber = true
 
--- line number
-o.number = true
-o.relativenumber = true
+-- Update time for Neovim
+vim.opt.updatetime = 200
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
