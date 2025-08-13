5bf722d786c5e3a22626a6b3f18301143dc6b8d5
be99inner <anurak.jann@gmail.com>
2025-08-13 16:41:51 +0700

    fix(nvim): remove yaml-companion plugin

--- config/nvim-vanila/lua/plugins/extras/ui.lua
+++ config/nvim-vanila/lua/plugins/extras/ui.lua
@@ -152,19 +152,6 @@ local plugins = {
     version = false,
     opts = require("configs.extras.ui.mini-notify"),
   },
-
-  -- Yaml schema
-  {
-    "someone-stole-my-name/yaml-companion.nvim",
-    dependencies = {
-        { "neovim/nvim-lspconfig" },
-        { "nvim-lua/plenary.nvim" },
-        { "nvim-telescope/telescope.nvim" },
-    },
-    config = function()
-      require("telescope").load_extension("yaml_schema")
-    end,
-  }
 }
 
 return plugins
