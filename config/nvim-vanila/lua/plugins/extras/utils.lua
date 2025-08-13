37a52040dd8f0e420d811861589ab900ba024c80
be99inner <anurak.jann@gmail.com>
2025-08-13 11:11:57 +0700

    fix(nvim): fix plugin to integrate with tmux

--- config/nvim-vanila/lua/plugins/extras/utils.lua
+++ config/nvim-vanila/lua/plugins/extras/utils.lua
@@ -3,15 +3,24 @@ local plugins = {
   {
     "alexghergh/nvim-tmux-navigation",
     event = "VeryLazy",
-    cmd = { "NvimTmuxNavigateLeft", "NvimTmuxNavigateRight", "NvimTmuxNavigateUp", "NvimTmuxNavigateDown" },
+    cmd = {
+      "NvimTmuxNavigateLeft",
+      "NvimTmuxNavigateRight",
+      "NvimTmuxNavigateUp",
+      "NvimTmuxNavigateDown",
+      "NvimTmuxNavigateLastActive",
+      "NvimTmuxNavigateNext",
+    },
     opts = {
       disabled_when_zoomed = true,
     },
     keys = {
-      { "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", { desc = "Tmux Navigate Left", silent = false } },
-      { "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", { desc = "Tmux Navigate Down", silent = false } },
-      { "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", { desc = "Tmux Navigate Up", silent = false } },
-      { "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", { desc = "Tmux Navigate Right", silent = false } },
+      { "<C-h>",     "<cmd>NvimTmuxNavigateLeft<cr>",       { desc = "Tmux Navigate Left", silent = true } },
+      { "<C-j>",     "<cmd>NvimTmuxNavigateDown<cr>",       { desc = "Tmux Navigate Down", silent = true } },
+      { "<C-k>",     "<cmd>NvimTmuxNavigateUp<cr>",         { desc = "Tmux Navigate Up", silent = true } },
+      { "<C-l>",     "<cmd>NvimTmuxNavigateRight<cr>",      { desc = "Tmux Navigate Right", silent = true } },
+      { "<C-\\>",    "<cmd>NvimTmuxNavigateLastActive<cr>", { desc = "Tmux Navigate To Last Active", silent = true } },
+      { "<C-Space>", "<cmd>NvimTmuxNavigateNext<cr>",       { desc = "Tmux Navigate Next", silent = true } },
     },
   },
 }
